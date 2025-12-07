a2676a6ee9d9b3ecb1df80acd64f86ebde29be05
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-12-08 01:53:29 +0530

    add splash screen implementation with custom logo and theme

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
