a170e68f6ca03b63f9dc39bd92f673771eb3a106
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-12-23 23:49:14 +0530

    add vector drawable resources and color definitions

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
@@ -13,17 +13,17 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeApp"
+            baseName = "shared"
             isStatic = true
         }
     }
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
@@ -47,25 +47,12 @@ kotlin {
 }
 
 android {
-    namespace = "org.samarth.nutrisport"
+    namespace = "org.samarth.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "org.samarth.nutrisport"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -73,7 +60,4 @@ android {
     }
 }
 
-dependencies {
-    debugImplementation(compose.uiTooling)
-}
 
---
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/Colors.kt
@@ -0,0 +1,38 @@
+package com.nutrisport.shared
+
+import androidx.compose.ui.graphics.Color
+
+val GrayLighter = Color(0xFFFAFAFA)
+val Gray = Color(0xFFF1F1F1)
+val GrayDarker = Color(0xFFEBEBEB)
+
+val Yellowish = Color(0xFFEEFF00)
+val Orange = Color(0xFFF24C00)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val TextPrimary = Black
+val TextSecondary = Orange
+val TextWhite = White
+val TextBrand = Yellowish
+
+val Surface = White
+val SurfaceLighter = GrayLighter
+val SurfaceDarker = Gray
+
+val ButtonPrimary = Yellowish
+val ButtonSecondary = GrayDarker
+val ButtonDisabled = GrayDarker
+
+val IconPrimary = Black
+val IconSecondary = Orange
+val IconWhite = White
+
+
+
+
+
+
+
+
+
